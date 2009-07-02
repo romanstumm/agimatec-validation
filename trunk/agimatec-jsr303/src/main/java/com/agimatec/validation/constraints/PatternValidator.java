7170b9861a568fd3d218d0c8a533d714492a6e97
donaldrwoods <donaldrwoods@4b8b91bf-f249-0410-85fa-df573acf1f6a>
2009-07-02 17:49:54 +0000

    start adding missing ASL 2.0 license headers.  added annotation parameter checks and ValidationException usage where needed.

--- trunk/agimatec-jsr303/src/main/java/com/agimatec/validation/constraints/PatternValidator.java
+++ trunk/agimatec-jsr303/src/main/java/com/agimatec/validation/constraints/PatternValidator.java
@@ -1,10 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
 package com.agimatec.validation.constraints;
+
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.ValidationException;
 import javax.validation.constraints.Pattern;
 
 /**
@@ -17,7 +38,11 @@ public class PatternValidator implements ConstraintValidator<Pattern, String> {
     protected java.util.regex.Pattern pattern;
 
     public void initialize(Pattern params) {
-        pattern = java.util.regex.Pattern.compile(params.regexp(), toInt(params.flags()));
+        try {
+            pattern = java.util.regex.Pattern.compile(params.regexp(), toInt(params.flags()));
+        } catch ( PatternSyntaxException e ) {
+            throw new ValidationException( "Invalid regular expression pattern", e );
+        }
     }
 
     private int toInt(Pattern.Flag[] flags) {
--- trunk/agimatec-jsr303/src/main/java/com/agimatec/validation/constraints/SizeValidator.java
+++ trunk/agimatec-jsr303/src/main/java/com/agimatec/validation/constraints/SizeValidator.java
@@ -1,7 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
 package com.agimatec.validation.constraints;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.ValidationException;
 import javax.validation.constraints.Size;
 import java.lang.reflect.Array;
 import java.util.Collection;
@@ -27,6 +46,12 @@ public class SizeValidator implements ConstraintValidator<Size, Object> {
     public void initialize(Size constraint) {
         min = constraint.min();
         max = constraint.max();
+        if (min < 0)
+            throw new ValidationException( "Min cannot be negative" );
+        if (max < 0)
+            throw new ValidationException( "Max cannot be negative" );
+        if (max < min)
+            throw new ValidationException( "Max cannot be less than Min" );
     }
 
     /**
